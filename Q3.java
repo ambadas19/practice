7b349de2ddfcb5ae59289383af8bd4e2f8c2e2a8
ambadas19 <deshmukhambadas510@gmail.com>
2023-02-26 20:25:24 +0530

    remove string

--- Q3.java
+++ Q3.java
@@ -3,8 +3,7 @@ public class Q3 {
     public static void main(String[] args) {
 
         String str1 = "2552";
-        String str2 ="";
-        String str3 = " ";
+        
 
         for(int i=str1.length()-1 ; i>=0 ; i--)
         {
